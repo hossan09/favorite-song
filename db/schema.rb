d687facba7742f0d3b31acc4cfee12dff4d266b4
hossan09 <hossan9.saito@gmail.com>
2023-01-21 15:55:35 +0900

    playlistモデルを作成しsongモデルtagモデルを作成
    songモデルを先に作成したらマイグレーションでエラーが発生
    tagモデルを作成しsongモデルの名前変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_15_155932) do
+ActiveRecord::Schema.define(version: 2023_01_21_064852) do
+
+  create_table "playlists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "songs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "artist", null: false
+    t.string "album", null: false
+    t.text "text"
+    t.string "link"
+    t.bigint "user_id", null: false
+    t.bigint "playlist_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["playlist_id"], name: "index_songs_on_playlist_id"
+    t.index ["tag_id"], name: "index_songs_on_tag_id"
+    t.index ["user_id"], name: "index_songs_on_user_id"
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +53,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_155932) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "songs", "playlists"
+  add_foreign_key "songs", "tags"
+  add_foreign_key "songs", "users"
 end
